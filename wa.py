71a053c7387169f26b730fd705c1c6a8b272e0ca
François Boulogne <fboulogne sciunto org>
2013-05-28 15:34:58 +0200

    add debug flag

--- wa.py
+++ wa.py
@@ -13,14 +13,6 @@ from subprocess import call
 
 import logging
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-
-
-steam_handler = logging.StreamHandler()
-steam_handler.setLevel(logging.DEBUG)
-logger.addHandler(steam_handler)
-
 
 def query_yes_no(question, default="yes"):
     """Ask a yes/no question via raw_input() and return their answer.
@@ -63,18 +55,32 @@ if __name__ == '__main__':
     #                                 epilog='')
     #parser.add_argument('--version', action='version', version=info.NAME + ' ' + info.VERSION)
     parser.add_argument('url', help='URL to archive', metavar='URL')
-    parser.add_argument('-d', help='Destination directory', metavar='DIR')
+    parser.add_argument('-o', help='Destination directory', metavar='DIR')
+    parser.add_argument('-d', '--debug', action='store_true',
+                        default=False, help='Run in debug mode')
     #parser.add_argument('--notimecheck', help='No timecheck', action='store_false')
 
     args = parser.parse_args()
 
-    if args.d is None:
-        args.d = os.path.expanduser('~/wiki/archive')
-        args.d = '/tmp/dir'
-    archive_root =  os.path.split(args.d)[0]
+    if args.debug:
+        llevel = logging.DEBUG
+    else:
+        llevel = logging.INFO
+    logger = logging.getLogger()
+    logger.setLevel(llevel)
+
+    steam_handler = logging.StreamHandler()
+    steam_handler.setLevel(llevel)
+    logger.addHandler(steam_handler)
+
+
+    if args.o is None:
+        args.o = os.path.expanduser('~/wiki/archive')
+        args.o = '/tmp/dir'
+    archive_root =  os.path.split(args.o)[0]
 
     name = uuid.uuid4()
-    (title, filepath) = archive.archive_to_markdown(args.d, name, args.url)
+    (title, filepath) = archive.archive_to_markdown(args.o, name, args.url)
 
     filepath_short = os.path.relpath(filepath, archive_root)
 
